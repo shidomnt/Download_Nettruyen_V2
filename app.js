65c621aee39e892518acdc988dd4952e4ec72ac9
shidomnt <hatranquang02@gmail.com>
2022-01-10 09:21:49 +0700

    get url from input file

--- app.js
+++ app.js
@@ -7,31 +7,43 @@ import {
   analysisDocumentToTarget,
 } from './helper.js';
 
-const urlManga = 'http://www.nettruyengo.com/truyen-tranh/quai-vat-khong-ten-25197';
-
-(async () => {
-  const rootPath = 'E:/';
-  const nameSaveTarget = 'information';
-  try {
-    console.time('>>> CRAWL TOTAL TIME');
-    console.time('>>> DOWNLOAD TOTAL TIME');
-    console.log('>>> Crawling from website...');
-    const document = await analysisURL(urlManga);
-    const target = await analysisDocumentToTarget(document);
-    target.data = target.data.reverse();
-    const parserTargetToJSON = JSON.stringify(target);
-    console.timeEnd('>>> CRAWL TOTAL TIME');
-    console.log('>>> Start download file...\n');
-    console.log('>>> ---------------------- <<<\n');
-    await handleOBJ(target, rootPath);
-    const pathSaveFile = `${rootPath}${removeAccents(target.nameManga)}/${nameSaveTarget}`
-    fs.mkdirSync(pathSaveFile, { recursive: true });
-    fs.writeFileSync(`${pathSaveFile}/desc.json`, parserTargetToJSON, { mode: 777 });
-    console.log('>>> Saved File!');
-    console.timeEnd('>>> DOWNLOAD TOTAL TIME');
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
+fs.readFile('./input.in', { encoding: 'utf8' }, function (err, data) {
+  if (err) {
+    console.log(err);
+    return;
   }
-})()
+  data = data.trim().split('\n');
+  data.forEach(async (line) => {
+    const urlManga = line.trim();
+    await (async () => {
+      const rootPath = `/home/tranha/workspace/nettruyen/`;
+      const nameSaveTarget = 'information';
+      try {
+        console.time('>>> CRAWL TOTAL TIME');
+        console.time('>>> DOWNLOAD TOTAL TIME');
+        console.log('>>> Crawling from website...');
+        const document = await analysisURL(urlManga);
+        const target = await analysisDocumentToTarget(document);
+        target.data = target.data.reverse();
+        const parserTargetToJSON = JSON.stringify(target);
+        console.timeEnd('>>> CRAWL TOTAL TIME');
+        console.log('>>> Start download file...\n');
+        console.log('>>> ---------------------- <<<\n');
+        await handleOBJ(target, rootPath);
+        const pathSaveFile = `${rootPath}${removeAccents(
+          target.nameManga
+        )}/${nameSaveTarget}`;
+        fs.mkdirSync(pathSaveFile, { recursive: true });
+        fs.writeFileSync(`${pathSaveFile}/desc.json`, parserTargetToJSON, {
+          mode: 777,
+        });
+        console.log('>>> Saved File!');
+        console.timeEnd('>>> DOWNLOAD TOTAL TIME');
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    })();
+  });
+});
